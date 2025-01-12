f3c5a47e03fad159bc04e77a787dd545e81bb7a5
TheBitStick <the@bitstick.rip>
2025-01-12 00:16:29 -0600

    Added font config

--- hosts/specialization/Jorges-Laptop-Ultra.nix
+++ hosts/specialization/Jorges-Laptop-Ultra.nix
@@ -65,6 +65,18 @@
     };
   };
 
+  fonts = {
+    fontconfig.useEmbeddedBitmaps = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+    ];
+  };
+
   programs.steam = {
     enable = true;
     localNetworkGameTransfers.openFirewall = true;
